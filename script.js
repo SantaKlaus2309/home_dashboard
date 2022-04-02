42397af1257a47d42755a8cbc55932b88e918161
SantaKlaus2309 <84734705+SantaKlaus2309@users.noreply.github.com>
2022-04-02 14:45:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -240,3 +240,40 @@ request.send();
 document.getElementById("switch-2").checked = true;
 document.getElementById("switch-1").checked = true;
 }
+
+function playMyAudiopp(){
+
+  var request = new XMLHttpRequest();
+
+  request.open('GET', 'https://blr1.blynk.cloud/external/api/update?token=7rzvV6nkV6SVnHafeVI3SV0x6IaPrXJS&v5=0');
+  
+  request.onreadystatechange = function () {
+  if (this.readyState === 4) {
+  console.log('Status:', this.status);
+  console.log('Headers:', this.getAllResponseHeaders());
+  console.log('Body:', this.responseText);
+  }
+  };
+  
+  request.send();
+
+}
+
+
+function pauseMyAudiopp(){
+  var request = new XMLHttpRequest();
+
+request.open('GET', 'https://blr1.blynk.cloud/external/api/update?token=7rzvV6nkV6SVnHafeVI3SV0x6IaPrXJS&v5=1');
+
+request.onreadystatechange = function () {
+if (this.readyState === 4) {
+console.log('Status:', this.status);
+console.log('Headers:', this.getAllResponseHeaders());
+console.log('Body:', this.responseText);
+}
+};
+
+request.send();
+       
+
+}
